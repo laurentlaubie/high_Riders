24ea7e72d3766dc066170fbb1eb63b8ab05a27e0
laurentlaubie <laurent.laubie31@gmail.com>
2021-09-22 21:16:51 +0200

    Bugfix/ create edit-add-delete methode => error message--DOING

--- high-riders/src/Entity/Spot.php
+++ high-riders/src/Entity/Spot.php
@@ -7,8 +7,6 @@ use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
@@ -272,6 +270,11 @@ class Spot
      * 
      */
     private $departement;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $slug;
 
     public function __construct()
     {
@@ -684,4 +687,17 @@ class Spot
 
         return $this;
     }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+    
 }
