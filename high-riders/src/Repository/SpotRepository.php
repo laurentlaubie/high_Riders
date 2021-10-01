bc57477a320ff37f40fb264585526a2360033a56
SebastienBesquentBlandenet <sebastien.besquent@gmail.com>
2021-10-01 08:13:05 +0200

    query builder to Search --DONE

--- high-riders/src/Repository/SpotRepository.php
+++ high-riders/src/Repository/SpotRepository.php
@@ -28,23 +28,13 @@ class SpotRepository extends ServiceEntityRepository
      * @param $title
      * @return Spot[]
      */
-    public function searchSpotByTitle($title)
+    public function searchSpotByTitle($value)
     {
-        dd($title);
-        $testquery = $this->createQueryBuilder('spot')
-        // Clause WHERE pour filtre en fonction de $title
-        ->where('spot.title LIKE :title')
-        ->setParameter('title', "%$title%")
-        ->orderBy('spot.createdAt', 'DESC')
-        ->getQuery()
-        ;
-        dd($testquery);
-
         // https://www.doctrine-project.org/projects/doctrine-orm/en/2.9/reference/query-builder.html
         return $this->createQueryBuilder('spot')
-            // Clause WHERE pour filtre en fonction de $title
+        // Clause WHERE pour filtre en fonction de $title
             ->where('spot.title LIKE :title')
-            ->setParameter(':title', "%$title%")
+            ->setParameter('title', "%$value%")
             ->orderBy('spot.createdAt', 'DESC')
             ->getQuery()
             ->getResult();
