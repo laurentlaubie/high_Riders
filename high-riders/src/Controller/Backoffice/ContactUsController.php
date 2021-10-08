300cc0de9b1b5bb68f27376e4cc45587e37b1491
laurentlaubie <laurent.laubie31@gmail.com>
2021-10-08 16:26:09 +0200

    feat/create contactUs methode and template for message--DONE

--- high-riders/src/Controller/Backoffice/ContactUsController.php
+++ high-riders/src/Controller/Backoffice/ContactUsController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller\Backoffice;
 
+use App\Repository\ContactusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+    /**
+     * @Route("/backoffice/contact/us", name="backoffice_")
+     */
 class ContactUsController extends AbstractController
 {
+    // ===================== Page contact Display  =================//
     /**
-     * @Route("/backoffice/contact/us", name="backoffice_contact_us")
+     * @Route("/", name="contact", methods={"GET"})
      */
-    public function index(): Response
+    public function index(ContactusRepository $contactusRepository): Response
     {
+        $contactUS = $contactusRepository->findAll();
+
         return $this->render('backoffice/contact_us/index.html.twig', [
-            'controller_name' => 'ContactUsController',
+            'ContactUs' => $contactUS,
         ]);
     }
 }
