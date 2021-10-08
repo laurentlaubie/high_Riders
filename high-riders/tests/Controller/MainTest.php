e499c55657553aa8422c886c1cd0bd996f03c4a3
SebastienBesquentBlandenet <sebastien.besquent@gmail.com>
2021-10-08 13:49:52 +0200

    sprint-3 08-10-2021

--- high-riders/tests/Controller/MainTest.php
+++ high-riders/tests/Controller/MainTest.php
@@ -5,29 +5,29 @@ namespace App\Tests\Controller;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class MainTest extends TestCase
+class MainTest extends WebTestCase
 {
-    // public function testLogin(): void
-    // {
-    //     // On simule l'accès à une la page /login
-    //     // via un navigateur intégré
-    //     $client = static::createClient();
-    //     $crawler = $client->request('GET', 'api/v1/login_check');
+    public function testLogin(): void
+    {
+        // On simule l'accès à une la page /login
+        // via un navigateur intégré
+        $client = static::createClient();
+        $crawler = $client->request('GET', 'backoffice');
 
-    //     // Est ce que la page répond correctement
-    //     $this->assertResponseIsSuccessful();
+        // Est ce que la page répond correctement
+        $this->assertResponseIsSuccessful();
 
-    //     // On vérifie également que la page de login possède bien
-    //     // une balise h1 avec le contenu "Please sign in"
-    //     // $this->assertSelectorTextContains('h1.h3', 'Please sign in');
-    // }
+        // On vérifie également que la page de login possède bien
+        // une balise h1 avec le contenu "Please sign in"
+        // $this->assertSelectorTextContains('h1.h3', 'Please sign in');
+    }
 
     // public function testRegister(): void
     // {
     //     // On simule l'accès à une la page /login
     //     // via un navigateur intégré
     //     $client = static::createClient();
-    //     $crawler = $client->request('GET', 'api/v1/users/add');
+    //     $crawler = $client->request('GET', 'backoffice/users/add');
 
     //     // Est ce que la page répond correctement
     //     $this->assertResponseIsSuccessful();
